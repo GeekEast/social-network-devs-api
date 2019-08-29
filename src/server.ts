b2ac0a42af0dd345ac2d674aceb801ee7f9f5edd
GeekEast <sx.ba2017@gmail.com>
2019-08-29 11:02:52 +0930

    update

--- src/server.ts
+++ src/server.ts
@@ -16,4 +16,4 @@ app.listen(port, () => {
 // get the uri for mongodb with username and password
 const username = config.get("Server.USER") as string;
 const access_key = config.get("Server.PASSWORD") as string;
-const url = (<string>config.get("Server.URL")).replace(/USER/,username).replace(/PASSWORD/,access_key);
+const uri = (<string>config.get("Server.URI")).replace(/USER/,username).replace(/PASSWORD/,access_key);
