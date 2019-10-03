5beb2f5b1df24e08a51f78bd027f600301297754
GeekEast <sx.ba2017@gmail.com>
2019-10-03 16:39:26 +0930

    udpate

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import express, { json } from 'express';
 import config from 'config';
 import { connectDB } from './services';
 import router from './middlewares/router';
+import cors from 'cors';
 const app = express();
 const port = config.get('Server.PORT');
 
 //  connect to mongodb
 connectDB();
 
+
 // mount necessary middlewars
 app.use(json()); // parse req.body from string to json object
-
+app.use(cors());
 // mount the top level router
 app.use('/api', router);
 
